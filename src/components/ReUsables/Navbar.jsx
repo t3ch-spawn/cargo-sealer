175ea0ffaa7817d1829b5df89e11f54e55a9054d
Tife <boluwatife1904@gmail.com>
2022-12-30 21:32:53 +0100

    edit

--- src/components/ReUsables/Navbar.jsx
+++ src/components/ReUsables/Navbar.jsx
@@ -5,51 +5,65 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="nav">
-      <div className="nav__icon">
-        <Link to="/">
-          <img src={NavIcon} alt="Logo" />
-        </Link>
-      </div>
+    <div className="navigation">
+      <Link to="/">
+        <img src={NavIcon} alt="" className="nav__icon--mobile" />
+      </Link>
 
-      <ul className="nav__list">
-        <li className="nav__item">
-          <a href="" className="nav__link">
-            Partner
-          </a>
-          <img src={Arrow} alt="" />
-        </li>
-        <li className="nav__item">
-          <a href="" className="nav__link">
-            Products
-          </a>
-          <img src={Arrow} alt="" />
-        </li>
-        <li className="nav__item">
-          <a href="" className="nav__link">
-            Company
-          </a>
-          <img src={Arrow} alt="" />
-        </li>
-        <li className="nav__item">
-          <a href="" className="nav__link">
-            Support
-          </a>
-          <img src={Arrow} alt="" />
-        </li>
-      </ul>
+      <input type="checkbox" className="nav__checkbox" id="nav-toggle" />
 
-      <div className="nav__cta">
-        <a href="#appInstall" className="nav__cta-link">
-          Get Started
-        </a>
-      </div>
+      <label htmlFor="nav-toggle" className="nav__button">
+        <span className="nav__burger"></span>
+      </label>
 
-      <div className="nav__dropDown nav__dropDown--partner"></div>
-      <div className="nav__dropDown nav__dropDown--products"></div>
-      <div className="nav__dropDown nav__dropDown--company"></div>
-      <div className="nav__dropDown nav__dropDown--support"></div>
-    </nav>
+      {/* <div className="nav__background"></div> */}
+
+      <nav className="nav">
+        <div className="nav__icon">
+          <Link to="/">
+            <img src={NavIcon} alt="Logo" />
+          </Link>
+        </div>
+
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="" className="nav__link">
+              Partner
+            </a>
+            <img src={Arrow} alt="" />
+          </li>
+          <li className="nav__item">
+            <a href="" className="nav__link">
+              Products
+            </a>
+            <img src={Arrow} alt="" />
+          </li>
+          <li className="nav__item">
+            <a href="" className="nav__link">
+              Company
+            </a>
+            <img src={Arrow} alt="" />
+          </li>
+          <li className="nav__item">
+            <a href="" className="nav__link">
+              Support
+            </a>
+            <img src={Arrow} alt="" />
+          </li>
+        </ul>
+
+        <div className="nav__cta">
+          <a href="#appInstall" className="nav__cta-link">
+            Get Started
+          </a>
+        </div>
+
+        <div className="nav__dropDown nav__dropDown--partner"></div>
+        <div className="nav__dropDown nav__dropDown--products"></div>
+        <div className="nav__dropDown nav__dropDown--company"></div>
+        <div className="nav__dropDown nav__dropDown--support"></div>
+      </nav>
+    </div>
   );
 }
 
