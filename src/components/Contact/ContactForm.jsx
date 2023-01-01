9f3a8a236cfac7bf07043ca6a486566dcc05a6fe
Tife <boluwatife1904@gmail.com>
2023-01-01 05:26:19 +0100

    edit

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 
 function ContactForm() {
+  function handleValidation(e) {
+    e.preventDefault();
+    checkInput();
+
+    function isEmail(email) {
+      return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+    }
+
+    function checkInput() {
+      const nameValue = document.querySelector("#name").value.trim();
+      const messageValue = document.querySelector("#message").value.trim();
+      const emailValue = document.querySelector("#email").value.trim();
+      const errorMsg1 = document.querySelector("#error-msg-1");
+      const errorMsg2 = document.querySelector("#error-msg-2");
+      const errorMsg3 = document.querySelector("#error-msg-3");
+
+      // validation for name
+      if (nameValue === "") {
+        errorMsg1.classList.add("active");
+      } else {
+        errorMsg1.classList.remove("active");
+      }
+
+      // validation for message
+      if (messageValue === "") {
+        errorMsg3.classList.add("active");
+      } else {
+        errorMsg3.classList.remove("active");
+      }
+
+      // validation for email
+
+      if (emailValue === "") {
+        errorMsg2.classList.add("active");
+
+        errorMsg2.textContent = "Email cannot be empty";
+      } else if (!isEmail(emailValue)) {
+        errorMsg2.classList.add("active");
+
+        errorMsg2.textContent = "This is not a valid email";
+      } else {
+        errorMsg2.classList.remove("active");
+      }
+    }
+  }
+
   return (
-    <form action="" className="contact__form">
+    <form action="" onSubmit={handleValidation} className="contact__form">
       <div className="contact__form-top">
         <div className="contact__form-input-group">
           <input
@@ -11,7 +57,9 @@ function ContactForm() {
             id="name"
             placeholder="Wade Warren"
           />
-          <small className="error-msg">email cannot be empty</small>
+          <small className="error-msg" id="error-msg-1">
+            name cannot be empty
+          </small>
           <label htmlFor="name" className="contact__form-label">
             Full Name
           </label>
@@ -25,6 +73,9 @@ function ContactForm() {
             id="email"
             placeholder="WadeWarren@gmail.com"
           />
+          <small className="error-msg" id="error-msg-2">
+            email cannot be empty
+          </small>
           <label htmlFor="email" className="contact__form-label">
             Email Address
           </label>
@@ -39,6 +90,9 @@ function ContactForm() {
             id="message"
             placeholder="Type your message"
           />
+          <small className="error-msg" id="error-msg-3">
+            message cannot be empty
+          </small>
           <label htmlFor="message" className="contact__form-label">
             Message
           </label>
--- src/components/Contact/ContactIndex.jsx
+++ src/components/Contact/ContactIndex.jsx
@@ -3,6 +3,9 @@ import Footer from "../ReUsables/Footer";
 import Navbar from "../ReUsables/Navbar";
 import ContactForm from "./ContactForm";
 import ContactImg from "../../images/contact-us.png";
+import ContactAddress from "../../images/contact-address.svg";
+import ContactMail from "../../images/contact-mail.svg";
+import ContactPhone from "../../images/contact-phone.svg";
 
 function ContactIndex() {
   return (
@@ -19,7 +22,35 @@ function ContactIndex() {
 
           <ContactForm />
 
-          <div className="contact__details"></div>
+          <div className="contact__details">
+            <div className="contact__details-item-container">
+              <img src={ContactAddress} alt="" />
+              <span>
+                <p className="contact__details-item-1">Address</p>
+                <p className="contact__details-item-2">
+                  1365 Wiley Road, suite 142, Schaumburg, IL 60173
+                </p>
+              </span>
+            </div>
+
+            <div className="contact__details-item-container">
+              <img src={ContactPhone} alt="" />
+              <span>
+                <p className="contact__details-item-1">Call us</p>
+                <p className="contact__details-item-2">+1 85 720 43 170</p>
+              </span>
+            </div>
+
+            <div className="contact__details-item-container">
+              <img src={ContactMail} alt="" />
+              <span>
+                <p className="contact__details-item-1">E-mail</p>
+                <p className="contact__details-item-2">
+                  info@cargodealerinc.com
+                </p>
+              </span>
+            </div>
+          </div>
         </div>
         <div className="contact__right">
           <div className="contact__imgContainer">
